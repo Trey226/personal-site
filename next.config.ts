2a69e1cf42c47701d84902eb6b28c2b19929ed46
Trey Gilliam <Bgilliam6@uco.edu>
2025-06-25 20:02:13 -0500

    rolled back whatever change this was because all I needed to do was force redeploy with build cache off

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: false,
-  experimental: {
-    optimizePackageImports: ['@mui/material', '@emotion/react', '@emotion/styled'],
-  },
 };
 
 export default nextConfig;
