aba46a3f4d4894e8145f3bc7a37ce1177116961d
Trey Gilliam <Bgilliam6@uco.edu>
2025-06-25 19:55:58 -0500

    updated next.js config to fix broken nav links in production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: false,
+  experimental: {
+    optimizePackageImports: ['@mui/material', '@emotion/react', '@emotion/styled'],
+  },
 };
 
 export default nextConfig;
