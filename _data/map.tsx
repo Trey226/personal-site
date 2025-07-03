00b39a2b440d288681dc406ae74a38758568e5f0
Trey Gilliam <Bgilliam6@uco.edu>
2025-07-02 21:02:38 -0500

    added dynamic coloring for the number of days I have spent per country

--- _data/map.tsx
+++ _data/map.tsx
@@ -1,22 +1,19 @@
-export type data = {
-    country: string;
-    value: number;
-}
+import type {  Data } from "react-svg-worldmap";
 
-export const datas = [
+export const datas: Data = [
     { country: "us", value: 9000 }, // united states
     { country: "kr", value: 125 }, // korea
     { country: "es", value: 55 }, // spain
     { country: "pe", value: 22 }, // peru
     { country: "mx", value: 15 }, // mexico
     { country: "ca", value: 13 }, // canada
-    { country: "ar", value: 5 }, // argentina
+    { country: "nl", value: 11 }, // netherlands
     { country: "fr", value: 9 }, // france
-    { country: "cl", value: 2 }, // chile
-    { country: "ch", value: 2 }, // switzerland
-    { country: "bz", value: 2 }, // belize
     { country: "tc", value: 8 }, // turks and caicos
     { country: "pt", value: 6 }, // portugal
-    { country: "nl", value: 11 }, // netherlands
+    { country: "ar", value: 5 }, // argentina
+    { country: "bz", value: 3 }, // belize
+    { country: "cl", value: 2 }, // chile
+    { country: "ch", value: 2 }, // switzerland
     { country: "jp", value: 2 }, // japan
   ];
--- components/WorldMap.tsx
+++ components/WorldMap.tsx
@@ -1,15 +1,39 @@
 'use client'
 import * as React from "react";
 import WorldMap from "react-svg-worldmap";
-import { data } from '@/_data/map';
 import { useState, useEffect } from "react";
+import type { CountryContext, Data } from "react-svg-worldmap";
 import "./css/Components.css";
 
 
 type MapDisplayProps = {
-  data: data[];
+    data: Data;
 };
 
+function getColor(days: number){
+if (days > 21)
+    return "#004D00";
+if (days > 7)
+    return "#4CAF50"
+else
+return "#A2D9A3";
+};
+
+
+const getStyle = ({
+    countryValue,
+    countryCode,
+    minValue,
+    maxValue,
+    color,
+}: CountryContext) => ({
+    fill:  getColor(countryValue!),
+    fillOpacity: countryValue ? 1 : 0,
+    stroke: "black",
+    strokeWidth: 1,
+    strokeOpacity: .3,
+    cursor: "auto",
+});
 
 export default function MapDisplay({ data }: MapDisplayProps) {
 
@@ -18,18 +42,16 @@ export default function MapDisplay({ data }: MapDisplayProps) {
     useEffect(() => {
         setScreenWidth(window.innerWidth);
     }, []);
-    
+
     return (
-            <div className="map-container">
-                <WorldMap
-                    color="blue"
-                    value-suffix="people"
-                    size={screenWidth < 1200 ? "responsive": "xxl"}
-                    data={data}
-                    valueSuffix="days"
-                    frame={true}
-                    strokeOpacity={.3}
-                />
-            </div>
+        <div className="map-container">
+            <WorldMap
+                styleFunction={getStyle}
+                size={screenWidth < 1200 ? "responsive" : "xxl"}
+                data={data}
+                valueSuffix="days"
+                frame={true}
+            />
+        </div>
     );
 }
