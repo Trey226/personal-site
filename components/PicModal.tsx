23a09fb2055151a8d7756fda37532b5390e26c50
Trey Gilliam <Bgilliam6@uco.edu>
2025-07-03 00:16:27 -0500

    prevented scroll while modal is open and changed the close button to be a red x for the time being

--- components/PicModal.tsx
+++ components/PicModal.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import "./Components.css";
 
 type PicModalProps = {
@@ -8,6 +9,17 @@ type PicModalProps = {
 
 export default function PicModal({ isOpen, name, close }: PicModalProps) {
 
+
+    //this useEffect prevents scrolling of the backpage while the modal is open
+    useEffect(() => {
+        if (isOpen) {
+          document.body.style.overflow = 'hidden';
+        }
+        return () => {
+          document.body.style.overflow = 'unset';
+        };
+      }, [isOpen]);
+
     if (!isOpen) {
         return null;
     }
@@ -16,7 +28,7 @@ export default function PicModal({ isOpen, name, close }: PicModalProps) {
         <div className="modal-overlay" /*onClick={close}*/>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 {/* Use the 'name' prop */}
-                <button onClick={close} className='modal-close'>close</button>
+                <button onClick={close} className='modal-close'>x</button>
                 <h1 className="modal-title">Pictures from {name}</h1>
                 <div className="modal-footer">small pics here</div>
             </div>
