c76008264dcd53022d832d5ef4bba4cc04756e55
Trey Gilliam <Bgilliam6@uco.edu>
2025-07-08 15:05:00 -0500

    geminis suggestion

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    outputFileTracingExcludes: {
-        '/api/**/*': [
-            './node_modules/**/*',
-            './app/**/*',
-            './components/**/*',
-            './public/**/*',
-            './_data/**/*',
-        ],
-    }
 }
 
 module.exports = nextConfig;
