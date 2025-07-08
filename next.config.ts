59394a0bd8e083327ddbcc933b08b245a725b660
Trey Gilliam <Bgilliam6@uco.edu>
2025-07-08 00:05:07 -0500

    ok hear me out. it actually should work this time

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    outputFileTracingExcludes: {
+        '/api/analyze': [
+            './node_modules/**/*',
+            './app/**/*',
+            './components/**/*',
+            './public/**/*',
+            './_data/**/*',
+        ],
+    }
 }
 
 module.exports = nextConfig;
