8584d3665c8648bc720b00cc9272af9b3b136cfb
Trey Gilliam <Bgilliam6@uco.edu>
2025-07-07 23:44:47 -0500

    maybe fixed my configs

--- declarations.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'exifr/dist/browser.mjs' {
-    import exifr from 'exifr';
-    export = exifr;
-  }
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
-module.exports = {
-     'api/analyze': ['./app/**/*'],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+}
 
+module.exports = {
+    outputFileTracingExcludes: {
+        'api/analyze': ['./app/**/*'],
+    }
 }
